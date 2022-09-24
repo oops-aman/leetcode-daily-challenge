963ec5e78640c26d6fe063bf1bd4d58811a4a11d
oops-aman <b200054@nitsikkim.ac.in>
2022-09-24 17:42:43 +0530

    113. Path Sum II

---
+++ path_sum_ii.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void dfs(TreeNode* root, int targetSum, vector<int> &path) {
+        if(!root) return;
+        
+        path.push_back(root -> val);
+        targetSum -= root -> val;
+        
+        if(!root -> left and !root -> right) {
+            if(targetSum == 0)
+                ans.push_back(path);
+        } else {
+            dfs(root -> left, targetSum, path);
+            dfs(root -> right, targetSum, path);
+        }
+        path.pop_back();
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<int> path;
+        dfs(root, targetSum, path);
+        return ans;
+    }
+};
