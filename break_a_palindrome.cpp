38e618b9610874884a531b26fdf5a7db2d12da73
oops-aman <b200054@nitsikkim.ac.in>
2022-10-11 19:13:44 +0530

    Break A Palindrome

---
+++ break_a_palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string breakPalindrome(string s) {
+        int n=s.size();
+        if(n<=1)return "";    
+        for(int i=0;i<n/2;++i){
+            if(s[i]!='a'){  
+                s[i]='a';
+                return s;
+            }
+        }
+        s.pop_back();  
+        s+='b';
+        return s;
+    }
+};
