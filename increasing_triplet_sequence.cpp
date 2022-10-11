4089bef65ea5d58ab56f23f2cb43fdcf3ba9b615
oops-aman <b200054@nitsikkim.ac.in>
2022-10-11 19:14:51 +0530

    Increasing Triplet Sequence

---
+++ increasing_triplet_sequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int fMax = INT_MAX, sMax = INT_MAX;
+        for(int i: nums) {
+            if(fMax >= i)
+                fMax = i;
+            else if(sMax >= i)
+                sMax = i;
+            else
+                return true;
+        }
+        return false;
+    }
+};
